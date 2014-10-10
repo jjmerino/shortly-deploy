3555533f46b95771679034a2366a8dee7e262ff5
Hack Reactor Students <accounts@hackreactor.com>
2014-10-09 17:12:28 -0700

    Mongoose refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     },
     injector: {
       options:{
-
+        ignorePath: 'public/'
       },
       prod: {
         files: {
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('./server-config.js');
-
+// var config = require('./app/config.js');
 var port = process.env.PORT || 4568;
 
 app.listen(port);
